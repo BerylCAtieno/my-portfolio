d7a38b4fe0f0a0a35c476fd0d38655aeac468d96
berylatieno30 <berylatieno30@gmail.com>
2024-12-11 16:14:09 +0000

    project dummy

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ navLinks.forEach(link => {
     link.classList.add('active');
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const containers = document.querySelectorAll('.project-container');
+  
+  containers.forEach((container, index) => {
+    // If index is odd, add the reverse class
+    if (index % 2 !== 0) {
+      container.classList.add('reverse');
+    }
+  });
+});
+
